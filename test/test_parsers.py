8487bd568a9b67b95d366cae6844ab84d9cf0442
juhopaak <juho.paakkonen@helsinki.fi>
2016-09-14 12:08:44 +0300

    Test class for parsers

--- test/test_parsers.py
+++ test/test_parsers.py
@@ -1,32 +1,38 @@
 import sys
 import os
+import importlib
 
 path = os.path.abspath('..')
 sys.path.append(path)
 
-from sites import *
-
-out = 'test.txt'
+out = 'test_file.txt'
+url_list = 'test_urls.txt'
 
 class TestParser:
     def test_file_exists(self):
-        urls = open('urls.txt', 'r')
-        for line in urls:
-            protocol,domain,path = line.split(".")
-            initialise_file(domain)
-        assert os.path.isfile(out) == True
+        urls = open(url_list, 'r')
+        for link in urls:
+            module = load_module(link)
+            initialise_file(module, link)
+            assert os.path.isfile(out) == True
+        urls.close()
 
     def test_file_not_empty(self):
-        urls = open('urls.txt', 'r')
-        for line in urls:
-            protocol,domain,path = line.split(".")
-            initialise_file(domain)
-        assert os.path.getsize(out) > 0
+        urls = open(url_list, 'r')
+        for link in urls:
+            module = load_module(link)
+            initialise_file(module, link)
+            assert os.path.getsize(out) > 0
+        urls.close()
 
     def test_file_contents_match(self):
         assert 1 == 1
 
-def initialise_file(domain):
+def load_module(link):
+    protocol,domain,path = link.split(".")
+    return importlib.import_module('sites.' + domain)
+
+def initialise_file(module, link):
     if ( os.path.isfile(out) ):
         os.remove(out)
-    domain.parse(url,file(out, 'w'))
+    module.parse(link,file(out, 'w'))
