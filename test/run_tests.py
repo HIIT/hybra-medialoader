57f7741547d15ebbf597d341d3e58bf58b3ded0f
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2016-09-21 12:02:09 +0300

    Class for running pytest tests

--- test/run_tests.py
+++ test/run_tests.py
@@ -12,24 +12,37 @@ url_list = 'urls.txt'
 def run_parser_tests():
     urls = open(url_list, 'r')
 
-    for url in urls:
-        domain = get_domain(url)
+    i = 1
+    for line in urls:
+        domain,url = line.split(",")
         module = importlib.import_module('sites.' + domain)
-        initialise_file(module, url)
-        pytest.main() # Pitäisi keksiä miten testeille annetaan argumentteja
-
+        initialise_file(module, url.strip())
+        print_log(domain, url.strip(), i)
+        pytest.main(['-q', '--domain=' + domain])
+        i += 1
     urls.close()
-
-def get_domain(url):
-    protocol,domain,path = url.split(".")
-    if (domain == 'blogit'): # The special case of Iltalehti blogs
-        domain = 'blogit_iltalehti'
-    return domain
+    print_not_tested()
 
 def initialise_file(module, url):
     if ( os.path.isfile(out) ):
         os.remove(out)
     module.parse(url, file(out, 'w'))
+
+def print_log(domain, url, test_no):
+    log_content = "\n**************"
+    log_content += "\nTest run no. " + str(test_no)
+    log_content += "\n**************\n"
+    log_content += "\nParser: " + domain
+    log_content += "\nURL: " + url
+    log_content += "\nTest content file path: test_contents/" + domain + ".txt"
+    log_content += "\nParser output file path: test_file.txt" + "\n"
+    print log_content
+
+def print_not_tested():
+    not_tested = open('not_tested.txt', 'r')
+    print "\nParsers that were not tested:"
+    for parser in not_tested:
+        print parser.strip()
 
 if __name__ == '__main__':
     run_parser_tests()
--- test/test_parsers.py
+++ test/test_parsers.py
@@ -1,48 +1,19 @@
 import sys
 import os
-import importlib
 import filecmp
 
 path = os.path.abspath('..')
 sys.path.append(path)
 
 out = 'test_file.txt'
-url_list = 'urls.txt'
 
 class TestParser:
 
     def test_file_exists(self):
-        urls = open(url_list, 'r')
-        for line in urls:
-            domain,url = line.split(",")
-            module = importlib.import_module('sites.' + domain)
-            initialise_file(module, url.strip())
-            if (os.path.isfile(out) == False):
-                print domain + " -> file does not exist"
-            assert os.path.isfile(out)
-        urls.close()
+        assert os.path.isfile(out)
 
     def test_file_not_empty(self):
-        urls = open(url_list, 'r')
-        for line in urls:
-            domain,url = line.split(",")
-            module = importlib.import_module('sites.' + domain)
-            initialise_file(module, url.strip())
-            if (os.path.getsize(out) == 0):
-                print domain + " -> file is empty"
-            assert os.path.getsize(out) > 0
-        urls.close()
+        assert os.path.getsize(out) > 0
 
-    def test_file_contents_match(self):
-        urls = open(url_list, 'r')
-        for line in urls:
-            domain,url = line.split(",")
-            module = importlib.import_module('sites.' + domain)
-            initialise_file(module, url.strip())
-            assert filecmp.cmp(out, 'test_contents/' + domain + ".txt")
-        urls.close()
-
-def initialise_file(module, url):
-    if ( os.path.isfile(out) ):
-        os.remove(out)
-    module.parse(url, file(out, 'w'))
+    def test_file_contents_match(self, domain):
+        assert filecmp.cmp(out, 'test_contents/' + domain + ".txt")
