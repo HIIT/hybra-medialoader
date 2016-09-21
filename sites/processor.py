1de4e654d1834802e6e8dd8e7919836078133d37
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2016-09-21 18:18:27 +0300

    Format processor class

---
+++ sites/processor.py
@@ -0,0 +1,15 @@
+# coding=utf8
+
+
+def process(content):
+    content = content.replace(' .', '.')
+    content = content.replace(' ,', ',')
+    content = content.replace('( ', '(')
+    content = content.replace(' )', ')')
+    content = content.replace(' !', '!')
+    content = content.replace(' ?', '?')
+    content = content.replace(' ;', ';')
+    content = content.replace('" ', '"')
+    content = content.replace(' '.decode('utf8'), ' ')
+    content = content.replace('  '.decode('utf8'), '')
+    return content
