44b0ac6c3a5e291f4137cdaf13ea03161ae2db42
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2016-09-28 17:10:57 +0300

    Update parsers

--- sites/aamulehti.py
+++ sites/aamulehti.py
@@ -2,21 +2,82 @@ import requests
 from bs4 import BeautifulSoup
 import processor
 
-def parse( url , out ):
+def parse( url ):
+
+	http_status = get_http_status( url )
 
 	r = requests.get( url )
 	r.encoding = 'UTF-8'
 	soup = BeautifulSoup( r.text, "html.parser" )
 
-	text = soup.find_all( class_ = 'Teksti' )
+	article = soup.find( class_ = 'article-content')
+	article.find( class_ = 'related-articles-container' ).decompose()
 
-	for div in text[0].find_all( 'div', {'class' : 'kuvavaraus-wrapper'} ):
+	title = str( article.find( class_ = 'Otsikko' ).get_text().strip().encode('utf8') )
+
+	category = str( article.find( class_ = 'category' ).get_text().strip() )
+
+	meta = article.find( class_ = 'post-meta' )
+	meta.find( class_ = 'category' ).decompose()
+	meta.find( class_ = 'updated' ).decompose()
+
+	meta_list = [None] * 4
+	i = 0
+	for string in meta.stripped_strings:
+		meta_list[i] = string
+		i += 1
+	date = [str( meta_list[2] ), str( meta_list[0] )]
+	time = [str( meta_list[3] ), str( meta_list[1] )]
+
+	images = article.find_all( 'img' )
+	image_src = [None] * len(images)
+	i = 0
+	for img in images:
+		image_src[i] = str( img['src'] )
+		i += 1
+
+	captions = article.find_all( class_ = 'caption' )
+	captions_text = [None] * len(captions)
+	i = 0
+	for caption in captions:
+		captions_text[i] = str( caption.get_text().encode('utf8') )
+		i += 1
+
+	text = article.find( class_ = 'Teksti' )
+	for div in text.find_all( class_ = 'kuvavaraus-wrapper' ):
 		div.decompose()
+	text = text.get_text(' ', strip = True)
+	text = processor.process(text)
+	text = str( text.encode('utf8') )
 
-	content = text[0].get_text(' ', strip=True)
-	content = processor.process(content)
+	media_content = { 'url' : url, 'http' : str(http_status), 'category' : category, 'date' : date, 'time' : time, 'title' : title, 'ingress' : '', 'text' : text, 'images' : image_src, 'captions' : captions_text}
 
-	out.write( content.encode('utf8') )
+	return media_content
+
+def get_http_status( url ):
+	r = requests.head( url )
+	return r.status_code
+
+def write_file( out, content ):
+	file_content = content['url'].encode('utf8') + "\n"
+	file_content += content['http'] + "\n"
+	file_content += content['category'].encode('utf8') + "\n"
+
+	for date in content['date']:
+		file_content += date.encode('utf8') + "\n"
+	for time in content['time']:
+		file_content += time.encode('utf8') + "\n"
+
+	file_content += content['title'] + "\n"
+	file_content += content['ingress'].encode('utf8') + "\n"
+	file_content += content['text'] + "\n"
+
+	for img in content['images']:
+		file_content += img.encode('utf8') + "\n"
+	for caption in content['captions']:
+		file_content += caption + "\n"
+
+	out.write( file_content.strip() )
 
 if __name__ == '__main__':
 
--- sites/lapinkansa.py
+++ sites/lapinkansa.py
@@ -1,8 +1,6 @@
 import requests
-
 from bs4 import BeautifulSoup
-
-# Lapin kansa on nykyään maksullinen lehti
+import processor
 
 def parse( url , out ):
 
@@ -10,10 +8,15 @@ def parse( url , out ):
 	r.encoding = 'UTF-8'
 	soup = BeautifulSoup( r.text, "html.parser" )
 
-	teksti = soup.find_all( class_ = 'news-excerpt' )
+	text = soup.find_all( class_ = 'Teksti' )
 
-	for string in teksti[0].stripped_strings:
-	        out.write( string.encode('utf8') + ' ' )
+	for img in text[0].find_all( class_ = 'kuvavaraus-wrapper' ):
+		img.decompose()
+
+	content = text[0].get_text(' ', strip = True)
+	content = processor.process(content)
+
+	out.write( content.encode('utf8') )
 
 if __name__ == '__main__':
 	parse("http://www.lapinkansa.fi/Lappi/1194944697007/artikkeli/kaunis+tykky+voi+olla+kavala+puille.html", file('lapinkansa.txt', 'w'))
