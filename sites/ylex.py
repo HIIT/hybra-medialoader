60350378bce1dcaacba1f3b78a1b76b45f9b1b7b
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2016-10-02 16:12:54 +0300

    Simple approach to YLEX using their API to collect news

--- sites/ylex.py
+++ sites/ylex.py
@@ -4,15 +4,20 @@ import processor
 
 def parse( url ):
 
-	r = requests.get( url )
+	api_path = 'http://yle.fi/ylex/api/article/'
+
+	_id = url.split( url )[-1]
+
+	r = requests.get( api_path + _id )
+	r = r.json()
 
 	http_status = r.status_code
 	if r.status_code == 404:
 		return
 
 	## ylex seems to be JS single page application; this approach can't handle it. just return here
-
-	return processor.create_dictionary(url, http_status, '', [''], [''], '', '', '', '', [''], [''])
+	authors = map( labda x:['name'] , r['authors'] )
+	return processor.create_dictionary(url, http_status, r['homesection']['name'], [ r['dateModified'], r['datePublished'] ], [''], authors, r['title'], r['lead'], r['text'], [''], [''])
 
 if __name__ == '__main__':
 	parse("http://yle.fi/ylex/uutiset/ylexaan_tulossa_politiikan_superviikot__myos_sina_voit_tentata_paattajia_ennen_vaaleja/3-7875448", file('yle.txt', 'w'))
