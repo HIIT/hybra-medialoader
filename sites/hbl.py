c02a8d7886e3549728243d7b4d73cd7f37557241
juhopaak <juho.paakkonen@helsinki.fi>
2016-10-05 18:41:44 +0300

    Update parsers

--- sites/hbl.py
+++ sites/hbl.py
@@ -13,19 +13,19 @@ def parse( url ):
 	r.encoding = 'UTF-8'
 	soup = BeautifulSoup( r.text, "html.parser" )
 
-	text = soup.find_all( 'article' )
-	text[0].find( id = 'articleimages' ).decompose()
-	text[0]('p')[-1].decompose()
-	text[0].find( id = 'nodefooter' ).decompose()
-	text[0].find( id = 'page-title' ).decompose()
-	text[0].find( id = 'publishedinfo' ).decompose()
-	text[0].find( id = 'article-controls' ).decompose()
-	for script in text[0].find_all('script'):
-		script.decompose()
-	text = text[0].get_text(' ', strip=True)
-	text = processor.process(text)
+	#text = soup.find_all( 'article' )
+	#text[0].find( id = 'articleimages' ).decompose()
+	#text[0]('p')[-1].decompose()
+	#text[0].find( id = 'nodefooter' ).decompose()
+	#text[0].find( id = 'page-title' ).decompose()
+	#text[0].find( id = 'publishedinfo' ).decompose()
+	#text[0].find( id = 'article-controls' ).decompose()
+	#for script in text[0].find_all('script'):
+	#	script.decompose()
+	#text = text[0].get_text(' ', strip=True)
+	#text = processor.process(text)
 
-	return processor.create_dictionary(url, http_status, [''], [''], '', '', '', text, [''], [''])
+	return processor.create_dictionary(url, http_status, [''], [''], '', '', '', '', [''], [''])
 
 if __name__ == '__main__':
 
