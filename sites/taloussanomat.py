376de79ef5482b3847590de35835639bc649d19f
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2016-10-26 13:14:01 +0300

    Fix taloussanomat parser

--- sites/taloussanomat.py
+++ sites/taloussanomat.py
@@ -17,15 +17,16 @@ def parse( url ):
 	article = soup.find( class_ = 'single-article' )
 	processor.decompose_all( article.find_all( 'script' ) )
 
+	categories = processor.collect_categories( soup.find( class_ = 'section-title' ), False )
 	datetime_list = processor.collect_datetime( article.find( 'time' ), '' )
 	author = processor.collect_text( article.find( class_ = 'byline' ), False )
 	title = processor.collect_text( article.find( itemprop = 'headline name' ), False )
 	ingress = processor.collect_text( article.find( class_ = 'ingress' ), False )
 	text = processor.collect_text( article.find( class_ = 'body' ), False )
-	images = processor.collect_images( article.find_all( 'img' ), '' )
+	images = processor.collect_images( article.find_all( 'img' ), 'src', '' )
 	captions = processor.collect_image_captions( article.find_all( 'figcaption' ) )
 
-	return processor.create_dictionary(url, r.status_code, [''], datetime_list, author, title, ingress, text, images, captions)
+	return processor.create_dictionary(url, r.status_code, categories, datetime_list, author, title, ingress, text, images, captions)
 
 if __name__ == '__main__':
 	parse("http://www.taloussanomat.fi/politiikka/2015/03/16/kreikka-lyhennamme-velkaamme-sovitusti-imflle/20153273/12", file('taloussanomat.txt', 'w'))
