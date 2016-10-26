445c5978e03e8e7403c156a7ff54d8862dc38731
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2016-10-26 13:13:28 +0300

    Fix ylex parser

--- sites/ylex.py
+++ sites/ylex.py
@@ -8,17 +8,34 @@ from datetime import datetime
 def parse( url ):
 
 	api_path = 'http://yle.fi/ylex/api/article/'
-
-	_id = url.split( url )[-1]
+	_id = url.split( '/' )[-1]
 
 	r = requests.get( api_path + _id )
-	r = r.json()
-
 	if r.status_code == 404:
 		processor.create_dictionary(url, r.status_code, [''], [''], '', '', '', '', [''], [''])
+	r.encoding = 'UTF-8'
 
-	authors = map( labda x:['name'] , r['authors'] )
-	return processor.create_dictionary(url, r.status_code, [r['homesection']['name']], [ r['dateModified'], r['datePublished'] ], authors, r['title'], r['lead'], r['text'], [''], [''])
+	json = r.json()
+
+	categories = [processor.process( json['homesection']['name'] )]
+
+	published = processor.process( json['datePublished'].replace( 'T', ' ' ).split( '+' )[0] )
+	updated = processor.process( json['dateModified'].replace( 'T', ' ' ).split( '+' )[0] )
+	datetime_list = [updated, published]
+
+	author = processor.process( json['authors'][0]['name'] )
+	title = processor.process( json['title'] )
+	ingress = processor.process( json['lead'] )
+
+	text_html = BeautifulSoup( json['html'], "html.parser" )
+	text = processor.collect_text( text_html, False )
+
+	image_json = json['image']
+	images = [image_json['uri'].encode('utf8')]
+	captions = [image_json['alt'].encode('utf8')]
+
+
+	return processor.create_dictionary( url, r.status_code, categories, datetime_list, author, title, ingress, text, images, captions)
 
 if __name__ == '__main__':
 	parse("http://yle.fi/ylex/uutiset/ylexaan_tulossa_politiikan_superviikot__myos_sina_voit_tentata_paattajia_ennen_vaaleja/3-7875448", file('yle.txt', 'w'))
