87c7899f7ed14d64f2015ce6363bf50e7d5b5008
juhopaak <juho.paakkonen@helsinki.fi>
2017-01-06 08:49:28 +0200

    Update yle_articles collector

--- sites/yle_articles.py
+++ sites/yle_articles.py
@@ -2,8 +2,8 @@ import requests
 
 def parse(api_request):
 
-    app_id = "f3365695"
-    app_key = "7010dcef0cf2393423e747473b6068c"
+    app_id = ""
+    app_key = ""
 
     example_request = "https://articles.api.yle.fi/v2/articles.json?published_after=2016-12-20T12:00:00%2b0300&offset=0&limit=10"
 
