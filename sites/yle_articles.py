e80b5099799d3d99ab0f776376ee486202e953a4
Arto Kekkonen <akekkonen@gmail.com>
2017-01-09 16:37:51 +0200

    Added draft of pagination

--- sites/yle_articles.py
+++ sites/yle_articles.py
@@ -7,7 +7,38 @@ from datetime import datetime
 import pickle
 import json
 
-def parse(api_request):
+def dump(data, file_name):
+
+    f = open(file_name + '.json', 'w')
+    json.dump(data, f)
+    f.close()
+
+    f = open(file_name + '.pickle', 'w')
+    pickle.dump(data, f)
+    f.close()
+
+def paging(api_request, max_iterations = 0):
+
+    ## Set max iterations to 0 if you want _everything_
+    max_iterations = max_iterations
+
+    ## Specify how many items will be returned with each call
+    item_limit = 1000
+
+    i = 0
+
+    while True:
+
+        news_items = parse(api_request = '', limit = item_limit, offset = i * item_limit)
+
+        dump(news_items, str(i))
+
+        if (len(news_items) < item_limit) or (max_iterations > 0 and i == max_iterations) :
+            break
+
+        i = i + 1
+
+def make_request(api_request, limit, offset):
 
     # Currently uses API's test environment
 
@@ -16,9 +47,15 @@ def parse(api_request):
 
     base_url = 'https://articles.api-test.yle.fi/v2/articles.json?'
 
-    api_request = base_url + api_request + '&app_id=' + app_id + '&app_key=' + app_key
+    api_request = base_url + api_request + '&limit=' + str(limit) + '&offset=' + str(offset) + '&app_id=' + app_id + '&app_key=' + app_key
 
     r = requests.get( api_request )
+
+    return r
+
+def parse(api_request, limit, offset):
+
+    r = make_request(api_request, limit, offset)
 
     news = r.json()['data']
 
@@ -30,11 +67,14 @@ def parse(api_request):
         url = data.get('url', {}).get('full', u'')
 
         ## News obtained via Articles API have tons of categories
-        categories = [x.get('title', {}).get('fi', u'') for x in data.get('subjects', [{}])]
+        ## Let's grab them all
 
+        categories = [x.get('title', {}).get('fi', u'') for x in data.get('subjects', [{}])]
         datetime_list = [data.get('datePublished', u''), data.get('dateContentModified', u'')]
         title = data.get('headline').get('full', u'')
         ingress = data.get('lead', u'')
+
+        ## This only gets the name of the first author
 
         author = data.get('authors', u'')
         if author is not u'':
@@ -44,11 +84,11 @@ def parse(api_request):
         text = [t['text'] for t in text]
         text = ' '.join(text)
 
-        # API only gives image ids, not urls
+        ## Articles API only gives Yle Images API ids, not urls
 
         images = filter(lambda x: x.get('type') == 'image', data.get('content'))
-        image_ids = [image['id'] for image in images]
-        image_captions = [image['alt'] for image in images]
+        image_ids = [image.get('id', u'') for image in images]
+        image_captions = [image.get('alt', u'') for image in images]
 
         dictionary_item = processor.create_dictionary(u'', url, r.status_code,
                                            categories, datetime_list, author,
@@ -59,4 +99,5 @@ def parse(api_request):
     return list_to_return
 
 if __name__ == '__main__':
-    parse('published_after=2016-12-20T12:00:00%2b0300&offset=0&limit=10&orderby=published desc')
+    ## Downloads 10 pages of 1000 items each and dumps them
+    paging('', 10)
