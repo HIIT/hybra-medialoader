dbf9d483e40f7d88ac57795a53394d6ced2187fb
Pääkkönen Juho <juho.paakkonen@helsinki.fi>
2017-03-16 14:37:26 +0200

    Parser for media archive

--- sites/kainuunsanomat.py
+++ sites/kainuunsanomat.py
@@ -35,5 +35,34 @@ def parse( url ):
 
 	return processor.create_dictionary('Kainuun sanomat', url, r.status_code, categories, datetime_list, author, title, ingress, text, images, captions)
 
+
+def parse_from_archive(url, content):
+
+	article = BeautifulSoup( content, "html.parser" )
+
+	if article == None:
+		return processor.create_dictionary('Kainuun sanomat', url, 404, [u''], [u''], u'', u'', u'', u'', [u''], [u''])
+
+	meta = article.find( class_ = 'hakutuloslahde' )
+
+	datetime_list = processor.collect_datetime( meta )
+
+	categories = [processor.collect_text( meta ).split(',')[1].strip()]
+
+	author = processor.collect_text( article.find( class_ = 'signeeraus' ) )
+
+	title = processor.collect_text( article.find( class_ = 'otsikko' ) )
+
+	text_divs = article.find_all( class_ = 'artikkelip')
+	text = ''
+	for text_content in text_divs:
+		text += processor.collect_text(text_content) + ' '
+	text = text.strip()
+
+	captions = processor.collect_image_captions( article.find_all( class_ = 'kuva') )
+
+	return processor.create_dictionary('Kainuun sanomat', url, 200, categories, datetime_list, author, title, u'', text, [u''], captions)
+
+
 if __name__ == '__main__':
 	parse("http://www.kainuunsanomat.fi/kainuun-sanomat/kotimaa/lipponen-moitti-sipilan-puheita-ministerien-vahentamisesta/", file('kainuunsanomat.txt', 'w'))
