0c937701770734ee10004232664587fe2a0fa690
Pääkkönen Juho <juho.paakkonen@helsinki.fi>
2017-09-04 17:59:50 +0300

    Fix broken parser

--- sites/aamuset.py
+++ sites/aamuset.py
@@ -14,20 +14,25 @@ def parse( url ):
 	r.encoding = 'UTF-8'
 	soup = BeautifulSoup( r.text, "html.parser" )
 
-	article = soup.find( class_ = 'view-news-item')
+	article = soup.find( 'article' )
 	if article == None:
 		return processor.create_dictionary('', url, r.status_code, [u''], [u''], u'', u'', u'', u'', [u''], [u''])
 
 	processor.decompose_all( article.find_all( 'script' ) )
 	processor.decompose_all( article.find_all( class_ = 'views-field-field-aamuset-related-images' ) )
 
-	categories = processor.collect_categories( article.find_all( class_ = 'views-field-field-aamuset-category') )
-	datetime_list = processor.collect_datetime( article.find( class_ = 'views-field-field-aamuset-category').parent.find_all('div')[3] )
-	author = processor.collect_text( article.find( class_  = 'views-field-field-visiting-journalist' ) )
-	title = processor.collect_text( article.find( class_ = 'views-field-title' ) )
-	text = processor.collect_text( article.find( class_ = 'views-field views-field-body' ) )
-	images = processor.collect_images( article.find_all( 'img' ), 'src', '' )
-	captions = processor.collect_image_captions( article.find_all( class_ = 'views-field-field-aamuset-caption-1' ) )
+	categories_element = soup.find( class_ = 'tsv3-c-as-articletags' )
+	categories = processor.collect_categories( categories_element.find_all( 'li' ) )
+
+	datetime_list = processor.collect_datetime( article.find( 'time' ) )
+
+	author = processor.collect_text( article.find( class_  = 'kirjoittaja' ) )
+	processor.decompose( article.find( class_  = 'kirjoittaja' ) )
+
+	title = processor.collect_text( article.find( class_ = 'otsikko' ) )
+	text = processor.collect_text( article.find( class_ = 'tsv3-c-as-article__textitem--teksti' ) )
+	images = processor.collect_images( article.find_all( 'img' ), 'src', 'http://www.aamuset.fi' )
+	captions = processor.collect_image_captions( article.find_all( class_ = 'tsv3-c-as-article__attachment__caption' ) )
 
 	return processor.create_dictionary('Aamuset', url, r.status_code, categories, datetime_list, author, title, u'', text, images, captions)
 
