3d5ae96009bfd3ee0520235a6ad4d40ba2238dad
Pääkkönen Juho <juho.paakkonen@helsinki.fi>
2017-10-05 11:14:08 +0300

    Fix broken parser

--- sites/talouselama.py
+++ sites/talouselama.py
@@ -14,24 +14,21 @@ def parse( url ):
 	r.encoding = 'UTF-8'
 	soup = BeautifulSoup( r.text, "html.parser" )
 
-	article = soup.find( 'article' )
+	article = soup.find( class_ = 'sc-feryYK fGYTpb' )
 	if article == None:
 		return processor.create_dictionary('', url, r.status_code, [u''], [u''], u'', u'', u'', u'', [u''], [u''])
 
-	processor.decompose_all( article.find_all( 'script' ) )
-	processor.decompose_all( article.find_all('noscript') )
-	processor.decompose( article.find( class_ = 'share-buttons' ) )
-	processor.decompose( article.find( class_ = 'subscribe-newsletter' ) )
+	processor.decompose_all( article.find_all( 'aside' ) )
 
-	categories = processor.collect_categories( article.find_all( class_ = 'kicker' ) )
-	datetime_list = processor.collect_datetime( article.find( class_ = 'meta' ) )
-	author = processor.collect_text( article.find( class_ = 'author' ) )
-	title = processor.collect_text( article.find( class_ = 'title' ) )
-	text = processor.collect_text( article.find( class_ = 'article-body' ) )
-	images = processor.collect_images( article.find_all( 'img' ), 'src', '')
+	categories = processor.collect_categories( [article.find( class_ = 'sc-bGbJRg fjsUfa' )] )
+	datetime_list = processor.collect_datetime( article.find( class_ = 'sc-likbZx fZfPtD' ) )
+	title = processor.collect_text( article.find( 'h1' ) )
+	ingress = processor.collect_text( article.find( class_ = 'sc-gbOuXE dCDvyT' ) )
+	text = processor.collect_text( article.find( class_ = 'sc-fyjhYU lmkwHD' ) )
+	images = processor.collect_images( [article.find( 'img' )], 'src', '')
 	captions = processor.collect_image_captions( article.find_all( 'figcaption' ) )
 
-	return processor.create_dictionary('Talouselämä', url, r.status_code, categories, datetime_list, author, title, u'', text, images, captions)
+	return processor.create_dictionary('Talouselämä', url, r.status_code, categories, datetime_list, u'', title, ingress, text, images, captions)
 
 if __name__ == '__main__':
 	parse("http://www.talouselama.fi/vaalit/vaalitebatti/murtuuko+suurten+puolueiden+valta++vaaliraati+vastaa/a2301513", file('talouselama.txt', 'w'))
